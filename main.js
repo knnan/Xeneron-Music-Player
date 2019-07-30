988259ebbd98e2611c5f88551acf5835e6dcf1bf
knnan <28503456+knnan@users.noreply.github.com>
2019-07-31 01:59:34 +0530

    first commit

--- main.js
+++ main.js
@@ -1,29 +1,108 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
+const url = require('url')
+const fs = require('fs')
+const mm = require('music-metadata')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+console.log('started the process');
+async function parseFiles ()
+{
+  try
+  {
+    console.time("jskdl");
+    Taginfos = []
+    pictures = []
+    let audioFiles = []
+    fs.readdirSync('/media/knnan/New Volume/Music/').forEach(file =>
+    {
+      if (path.parse(file).ext === '.mp3')
+      {
+        // console.log(file);
+        str1 = '/media/knnan/New Volume/Music/' + String(file)
+        audioFiles.push(str1)
+      }
+    });
+    // console.log(audioFiles);
 
-function createWindow () {
+    metadata = [];
+    count = 0;
+    for (const audioFile of audioFiles)
+    {
+
+      // await will ensure the metadata parsing is completed before we move on to the next file
+      metadata = await mm.parseFile(audioFile);
+      // console.log(metadata.common.title)
+      metadata.common.picture[ 0 ].data = metadata.common.picture[ 0 ].data.toString('base64');
+      Taginfos.push(metadata);
+      // music_text += `${metadata.common.title} - ${metadata.common.artist}\n`;
+      // console.log(music_text);
+      // pictures.push(metadata.common.picture)
+      // Do great things with the metadata
+    }
+    console.timeEnd("jskdl");
+
+    console.log("Total files parsed = ", Taginfos.length);
+    console.log("this is the result      ", Taginfos[ 0 ].common.title);
+    return Taginfos;
+  }
+  catch (error)
+  {
+    console.log(error);
+  }
+}
+
+
+
+
+// parseFiles();
+function createWindow ()
+{
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.on('did-finish-load', () =>
+  {
+    console.log("printing the result");
+    let result = parseFiles();
+    result.then((data) =>
+    {
+
+      // console.log("final result si        -------------------------- ", result);
+      mainWindow.webContents.send('secretmessage', data);
+    });
+
+  })
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function ()
+  {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -31,19 +110,48 @@ function createWindow () {
   })
 }
 
+
+
+
+
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item'
+      },
+      {
+        label: 'ClearItem'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click ()
+        {
+          app.quit();
+        }
+      }
+    ]
+  } ];
+
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function ()
+{
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
-app.on('activate', function () {
+app.on('activate', function ()
+{
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
--- renderer.js
+++ renderer.js
@@ -1,3 +1,130 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const electron = require('electron')
+const { ipcRenderer } = electron;
+const fs = require('fs');
+console.log(__dirname);
+
+ipcRenderer.send('getfiles');
+htmlout = '';
+musicdiv = document.querySelector('.musiccom');
+
+
+
+ipcRenderer.on('secretmessage', function (e, data)
+{
+    // console.log(data);
+    data.forEach((element, index) =>
+    {
+        // console.log(element);
+        htmlout = `
+        <div id="musiccontainerno${index}" class="musiccontainer">
+        
+        <div class="imagecontainer">
+        
+        <img id="trackimgno${index}" src="data:image/png;base64,${element.common.picture[ 0 ].data}"
+        alt="Cover art">
+        
+        </div>
+        <div class="track-info">
+        <p>${element.common.title}</p>
+        <h5>${element.common.artist}</h5>
+        <button class="trackcontrol" id="${index}" onclick="startsong(this.id)">play</button>
+        <button class="trackcontrol" id="stop${index}" onclick="stopsong(this.id)">stop</button>
+        </div>
+        <div>
+        
+        </div>
+        </div>
+        `
+        var g = document.createElement('div');
+        g.id = `musiccontainerno${index}`
+        g.setAttribute("class", "musiccontainer")
+
+        g.innerHTML = htmlout
+        musicdiv.appendChild(g);
+    });
+
+});
+
+
+
+function startsong (clicked)
+{
+    try
+    {
+
+        let mucref = document.getElementById(`musiccontainerno${clicked}`);
+        var player = document.getElementById(`musicaudio`);
+        document.getElementById('idtrack').innerText = clicked;
+
+
+
+        imgref = mucref.querySelector('img');
+        npimg = document.getElementById('nowplaying-img');
+        npimg.src = imgref.src;
+
+        titleref = mucref.querySelector('p');
+        artistref = mucref.querySelector('h5');
+
+        npinforef = document.getElementById('nowplayinginfo');
+        npinforef.querySelector('p').innerText = titleref.innerText;
+        npinforef.querySelector('h5').innerText = artistref.innerText;
+
+        musicid = `music_playerid${clicked}`;
+        document.getElementById('play-pause').innerText = "pause_circle_outline";
+        player.querySelector('source').src = `/media/knnan/New Volume/Music/${titleref.innerText}.mp3`;
+        player.load();
+        player.play();
+    }
+    catch (err)
+    {
+        console.log(err);
+    }
+}
+
+function stopsong (clicked)
+{
+    document.getElementById('play-pause').innerText = "play_circle_outline";
+    var player = document.getElementById(`musicaudio`);
+    player.pause();
+
+}
+function changplayicon ()
+{
+    console.log(document.getElementById('play-pause').innerText);
+    if (document.getElementById('play-pause').innerText == "play_circle_outline")
+    {
+
+        document.getElementById('play-pause').innerText = "pause_circle_outline";
+        var player = document.getElementById(`musicaudio`);
+        player.play();
+    }
+    else
+    {
+
+        document.getElementById('play-pause').innerText = "play_circle_outline";
+        var player = document.getElementById(`musicaudio`);
+        player.pause();
+    }
+}
+
+function nextsong ()
+{
+    let trackid = Number(document.getElementById('idtrack').innerText);
+    trackid += 1;
+    trackid = String(trackid);
+    document.getElementById('idtrack').innerText = trackid;
+    startsong(trackid);
+}
+function prevsong ()
+{
+    trackid2 = Number(document.getElementById('idtrack').innerText);
+    trackid2 -= 1;
+    trackid2 = String(trackid2);
+    document.getElementById('idtrack').innerText = trackid2;
+    startsong(trackid2);
+}
+
+
